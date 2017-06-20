cdfd05b024794aae6e0d51ecb5024bb5fd36de13
Gabriel Russell <TheBlargbla@gmail.com>
2017-06-20 00:51:39 -0400

    This works. Kinda
    Doesn't look pretty, some things broken for any reason I couldn't find.
    
    But it works. Kinda. I'll be fixing it tomorrow.

--- testing/pkinstall_source/pk/source/main.c
+++ testing/pkinstall_source/pk/source/main.c
@@ -1,33 +1,33 @@
 #include <string.h>
 #include <3ds.h>
 #include <stdio.h>
+#include <sys/stat.h> 
+#include <sys/types.h> 
+
 #include "functions.h"
 
-extern int cursor = 0;
+extern int cursor = 5;
 extern int screen = 0;
 
 int main()
 {
+	httpcInit(0);
 	gfxInitDefault();
 	PrintConsole topScreen, bottomScreen;
 	
 	consoleInit(GFX_TOP, &topScreen);
 	consoleInit(GFX_BOTTOM, &bottomScreen);
-	// Main loop
+
 	while (aptMainLoop())
 	{
 		gspWaitForVBlank();
 		hidScanInput();
 
-		//Your code goes here
-
+		
 		u32 kDown = hidKeysDown();
 		if (kDown & KEY_START)
-			break; //Break in order to return to hbmenu
+			break; 
 
-		//Select Screens
-	
-		//Choose which screen to print to (top)
 		consoleSelect(&topScreen);
 		
 		moveCursor();
@@ -45,25 +45,23 @@ int main()
 		
 		
 			if((cursor == 5) & (kDown & KEY_A)) {
-				screen = 1;
+				screen = 4;
 				cursor = 0;
+				consoleClear();
 			} else if ((cursor = 6) & (kDown & KEY_A)) {
-				screen = 2; 
+				screen = 4; 
 				cursor = 0;
+				consoleClear();
 			} else if ((cursor = 7) & (kDown & KEY_A)) {
-				screen = 3;
+				screen = 4;
 				cursor = 0;
+				consoleClear();
 			} else if ((cursor = 8) & (kDown & KEY_A)) {
 				screen = 4;
 				cursor = 0;
+				consoleClear();
 			}
-			
-		} else if (screen == 1) {
-			printf("Incomplete, will not work. Unfunctional.");
-		} else if (screen == 2) {
-			printf("Incomplete, will not work.");
-		} else if (screen == 3) {
-			printf("Incomplete, will not work.");
+	
 		} else if (screen == 4) {
 			printf("\x1b[1;22Tools\e[K\n");
 		
@@ -74,8 +72,12 @@ int main()
 			printStatement("2. FTPD II Turbo", 6);
 			
 			if ((cursor == 5) & (kDown & KEY_A)) {
+				mkdir("/3ds/", 0777);
+				mkdir("/3ds/JKSM/", 0777);
 				downloadToFile("https://github.com/J-D-K/JKSM/blob/master/JKSM.3dsx?raw=true", "/3ds/JKSM/JKSM.3dsx");
 			} else if ((cursor == 6) & (kDown & KEY_A)) {
+				mkdir("/3ds/", 0777);
+				mkdir("/3ds/FTPD", 0777);
 				downloadToFile("https://github.com/mtheall/ftpd/releases/download/v2.2/ftpd.3dsx", "/3ds/FTPD/FTPD.3dsx");
 			}
 		
