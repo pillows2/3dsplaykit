c192c35fbdf54572d148c2dc98eb5993af33aa88
Gabriel Russell <TheBlargbla@gmail.com>
2017-07-02 23:43:00 -0400

    Add two items
    Technically SCRTool doesn't work on purpose. I can't get it to compile for some reason...

--- testing/pkinstall_source/pk/source/main.c
+++ testing/pkinstall_source/pk/source/main.c
@@ -61,13 +61,15 @@ int main()
 			printf("\x1b[1;1HTools\e[K\n");
 		
 			moveCursor();
-			resetCursor(5, 9);
+			resetCursor(5, 11);
 			
 			printStatement("1. Full Install", 5);
 			printStatement("2. JKSM", 6);
 			printStatement("3. FTPD", 7);
 			printStatement("4. Notepad3DS", 8);
 			printStatement("5. CTRMUS", 9);
+			printStatement("6. SCRTool", 10);
+			printStatement("7. CTRXplorer", 11);
 			
 			if ((hidKeysDown() & KEY_A) & (cursor == 5)) {
 				mkdir("/3ds/", 0777);
@@ -75,12 +77,15 @@ int main()
 				mkdir("/3ds/FTPD/", 0777);
 				mkdir("/3ds/Notepad3DS", 0777);
 				mkdir("/3ds/CTRMus", 0777);
+				mkdir("/3ds/SCRTool (WIP, not working)", 0777);
+				mkdir("/3ds/CTRXplorer", 0777);
+				
 				consoleSelect(&bottomScreen);
 				downloadToFile("https://github.com/J-D-K/JKSM/blob/master/JKSM.3dsx?raw=true", "/3ds/JKSM/JKSM.3dsx");
 				downloadToFile("https://github.com/mtheall/ftpd/releases/download/v2.2/ftpd.3dsx", "/3ds/FTPD/FTPD.3dsx");
 				downloadToFile("https://github.com/RMcTn/Notepad3DS/releases/download/v1.1/notepad3DS.3dsx", "/3ds/Notepad3DS/Notepad3DS.3dsx");
 				downloadToFile("https://github.com/pillows2/pkinstall/blob/master/download/ctrmus.3dsx?raw=true", "/3ds/CTRMUS/CTRMUS.3dsx");
-
+				downloadToFile("https://github.com/pillows2/pkinstall/blob/master/download/CTRXplorer.3dsx?raw=true", "/3ds/CTRXplorer/CTRXplorer.sdmh");
 				resetScreen();
 				consoleSelect(&topScreen);
 			
@@ -103,6 +108,11 @@ int main()
 				mkdir("/3ds/", 0777);
 				mkdir("/3ds/CTRMus/", 0777);
 				downloadToFile("https://github.com/pillows2/pkinstall/blob/master/download/ctrmus.3dsx?raw=true", "/3ds/CTRMUS/CTRMUS.3dsx");
+				resetScreen();
+			} else if ((hidKeysDown() & KEY_A) & (cursor == 11)) {
+				mkdir("/3ds/", 0777);
+				mkdir("/3ds/CTRXplorer/", 0777);
+				downloadToFile("https://github.com/pillows2/pkinstall/blob/master/download/CTRXplorer.3dsx?raw=true", "/3ds/CTRXPlorer/CTRXplorer.3dsx");
 				resetScreen();
 			}
 			
